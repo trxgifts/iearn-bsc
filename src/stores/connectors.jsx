a8d5eb5591cb002e69ffd75e8e575f262a80e77f
TRXGIFTS <73261483+trxgifts@users.noreply.github.com>
2021-04-30 16:29:57 +0700

    Update connectors.jsx

--- src/stores/connectors.jsx
+++ src/stores/connectors.jsx
@@ -14,7 +14,7 @@ import { AuthereumConnector } from "@web3-react/authereum-connector";
 const POLLING_INTERVAL = 12000;
 const RPC_URLS = {
   1: "https://eth-mainnet.alchemyapi.io/v2/k2--UT_xVVXMOvAyoxJYqtKhlmyBbqnX",
-  4: "https://rinkeby.infura.io/v3/bd80ce1ca1f94da48e151bb6868bb150"
+  56: "https://bsc-dataseed1.ninicoin.io/"
 };
 
 export const injected = new InjectedConnector({
@@ -28,26 +28,26 @@ export const injected = new InjectedConnector({
 // });
 
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: RPC_URLS[1] },
+  rpc: { 56: RPC_URLS[56] },
   bridge: "https://bridge.walletconnect.org",
   qrcode: true,
   pollingInterval: POLLING_INTERVAL
 });
 
 export const walletlink = new WalletLinkConnector({
-  url: RPC_URLS[1],
+  url: RPC_URLS[56],
   appName: "yearn.finance"
 });
 
 export const ledger = new LedgerConnector({
-  chainId: 1,
-  url: RPC_URLS[1],
+  chainId: 56,
+  url: RPC_URLS[56],
   pollingInterval: POLLING_INTERVAL
 });
 
 export const trezor = new TrezorConnector({
-  chainId: 1,
-  url: RPC_URLS[1],
+  chainId: 56,
+  url: RPC_URLS[56],
   pollingInterval: POLLING_INTERVAL,
   manifestEmail: "dummy@abc.xyz",
   manifestAppUrl: "https://8rg3h.csb.app/"
@@ -57,19 +57,19 @@ export const frame = new FrameConnector({ supportedChainIds: [1] });
 
 export const fortmatic = new FortmaticConnector({
   apiKey: "pk_live_F95FEECB1BE324B5",
-  chainId: 1
+  chainId: 56
 });
 
 export const portis = new PortisConnector({
   dAppId: "5dea304b-33ed-48bd-8f00-0076a2546b60",
-  networks: [1, 100]
+  networks: [56, 100]
 });
 
 export const squarelink = new SquarelinkConnector({
   clientId: "5f2a2233db82b06b24f9",
-  networks: [1, 100]
+  networks: [56, 100]
 });
 
-export const torus = new TorusConnector({ chainId: 1 });
+export const torus = new TorusConnector({ chainId: 56 });
 
-export const authereum = new AuthereumConnector({ chainId: 1 });
+export const authereum = new AuthereumConnector({ chainId: 56 });
